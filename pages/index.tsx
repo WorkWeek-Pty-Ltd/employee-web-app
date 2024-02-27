0338a91982bfdae17557abbb897f3a907cd27935
Tristan Drummond <tristdrum@gmail.com>
2024-02-27 14:50:35 +0200

    Refactor import statements in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Layout from '../components/Layout';
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
 
 const Home = () => {
   const router = useRouter();
@@ -13,15 +13,15 @@ const Home = () => {
   }, [router]);
 
   return (
-    <Layout pageTitle="Welcome">
+    <Layout pageTitle="No Organisation Id">
       <div className="container mx-auto p-4">
         <h1 className="text-center text-2xl font-bold mt-5">
           Welcome to the Workweek Employee Web App
         </h1>
         <h3 className="mt-5">
-          Please select an organisation:
+          No organisation ID found. Please use the link provided by your
+          employer.
         </h3>
-        <p className="text-center mt-5">Redirecting to your organisation...</p>
       </div>
     </Layout>
   );
