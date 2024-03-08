3b95f4f2ba1bf9b9891f86f0fb7c76c7b46f4956
Tristan Drummond <tristdrum@gmail.com>
2024-03-08 09:16:39 +0200

    Update site button styling in SitesPage

--- pages/organisation/[organisationId]/index.tsx
+++ pages/organisation/[organisationId]/index.tsx
@@ -66,7 +66,7 @@ const SitesPage = () => {
             <button
               key={site.id}
               onClick={() => handleSiteClick(site.id)}
-              className="bg-white text-left w-full p-4 mb-2 rounded-lg flex items-center justify-between hover:bg-gray-100 border-purple-300 border"
+              className="bg-white text-left w-full pl-4 p-2 mb-2 rounded-lg flex items-center justify-between hover:bg-gray-100 border-purple-300 border"
             >
               <span className="text-gray-800 font-semibold">{site.name}</span>
               <span className="text-gray-500">
