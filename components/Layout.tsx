dc1945947ef105a605906fba11012f9f48899625
Tristan Drummond <tristdrum@gmail.com>
2024-03-08 14:02:13 +0200

    Update page title and main container styling in Layout component (to fix footer sticking to the bottom)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({
   children,
-  pageTitle = "Page Title",
+  pageTitle = "WorkWeek",
   backRoute,
 }) => {
   return (
@@ -26,7 +26,9 @@ const Layout: React.FC<LayoutProps> = ({
         <img src="/workweek.svg" alt="Workweek" />
         <h1 style={{ marginTop: "10px" }}>{pageTitle}</h1>
       </header>
-      <main className="container mx-auto mt-4 mb-4 px-4">{children}</main>
+      <main className="flex-1 container mx-auto mt-4 mb-4 px-4">
+        {children}
+      </main>
       <footer className="bg-light-grey text-black py-2 px-4 text-center text-xs sm:py-4 sm:px-6 sm:text-sm">
         © {new Date().getFullYear()} Workweek Employee Web App
       </footer>
